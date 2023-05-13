4e2948052bf32e9ec09cdd5973d6e23938839765
Amirhassan Usefi <Amirhassan5303@gmail.com>
2023-05-13 12:15:21 +0200

    add Team class to Main

--- src/Person.java
+++ src/Person.java
@@ -7,21 +7,21 @@ public class Person {
     private int birthDate;
 
     public void readInfo(){
-        System.out.print("Enter name: ");
-        name = console.next();
-        System.out.print("Enter last name: ");
-        lastName = console.next();
-        System.out.print("Enter national ID: ");
-        nationalId = console.nextInt();
-        System.out.print("Entre birth date: ");
-        birthDate = console.nextInt();
-    }
+    System.out.print("Enter name: ");
+    name = console.next();
+    System.out.print("Enter last name: ");
+    lastName = console.next();
+    System.out.print("Enter national ID: ");
+    nationalId = console.nextInt();
+    System.out.print("Entre birth date: ");
+    birthDate = console.nextInt();
+}
 
-    public void showInfo(){
-        System.out.println("name: " + name);
-        System.out.println("last name: " + lastName);
-        System.out.println("national ID: " + nationalId);
-        System.out.println("birth date: " + birthDate);
-    }
+public void showInfo(){
+    System.out.println("name: " + name);
+    System.out.println("last name: " + lastName);
+    System.out.println("national ID: " + nationalId);
+    System.out.println("birth date: " + birthDate);
+}
 
 }
--- src/Team.java
+++ src/Team.java
@@ -1,20 +1,17 @@
 public class Team {
     public static void main(String[] args){
         Player [] players = new Player[11];
-        Coach [] coach = new Coach[1];
+        Coach coach = new Coach();
 
-        public void readTeamInfo(){
-            for(int i = 0; i < players.length ; i++){
-                players[i] = new Player();
-                players[i].readInfo();
-                players[i].readPosition();
-            }
+
+        for(int i = 0; i < players.length ; i++){
+            players[i] = new Player();
+            players[i].readInfo();
+            players[i].readPosition();
         }
 
-        coach[0] = new Coach();
-        coach[0].readInfo();
-        coach[0].readCoachInfo();
+        coach.readInfo();
+        coach.readCoachInfo();
+
     }
-
-
 }
